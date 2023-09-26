f9aed34a74db3edb64250049cdef99545b5dadcc
floren2023 <florentavakar@gmail.com>
2023-09-27 00:39:20 +0200

    Search con nombre

--- src/main/java/com/curso/ecommerce/controller/HomeController.java
+++ src/main/java/com/curso/ecommerce/controller/HomeController.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -154,6 +155,15 @@ public class HomeController {
 		detalles.clear();
 		
 		return "redirect:/";
+	}
+	
+	@PostMapping("/search")
+	public String searchProduct(@RequestParam String nombre,Model model) {
+	
+		List<Producto> productos= productoService.findAll().stream().filter(p->p.getName().contains(nombre)).collect(Collectors.toList());
+	
+		model.addAttribute("productos",productos);
+		return "usuario/home";
 	}
 	
 }
